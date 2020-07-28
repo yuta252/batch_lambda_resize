e3625bcc732ac0c19fe3bb52bc38de4600d40655
Yuta Nakano <nakano.yuta252@gmail.com>
2020-07-28 22:38:48 +0900

    Lambda関数作成テスト

---
+++ README.rst
@@ -0,0 +1,115 @@
+===============================================
+Lambda関数によるS3アップロード画像のExif削除とリサイズ
+===============================================
+
+目的
+=====
+アップロードされた画像をDeepLearningで学習するためには画像のサイズとExif情報を削除する必要がある。
+特にモバイル端末で撮影した画像をStartlens（Web画面）から直接アップロードしてしまうとExif情報が付与されたままアップロードされてしまう。
+このExif情報を付与したまま学習してしまうと画像の回転が考慮されず学習の精度が下がってしまうためバッチ処理が必要である。
+
+ツールのバージョン
+==============
+:Python:    3.6.10
+:pip:       20.1.1
+
+
+インストールと起動方法
+==================
+リポジトリーからコードを取得し、venv環境に依存ライブラリをインストールする
+
+    #. $ git clone https://github.com/
+    #. $ cd startlens
+    #. $ python3.6 -m venv venv
+    #. $ source venv/bin/activate
+    #. (venv) $ pip install -r requirements/test.txt
+    #. (venv) $ python3.6 manage.py runserver --settings=config.settings.test
+
+
+AWSでの環境設定と起動方法
+======================
+:Ubuntu:    18.04
+:インスタンスタイプ:    t2.micro
+
+EC2インスタンス内での実行手順
+
+    #. sudo apt -y update
+    #. sudo apt -y upgrade
+    #. sudo apt -y install build-essential python3-dev libsqlite3-dev libreadline6-dev libgdbm-dev zlib1g-dev libbz2-dev sqlite3 tk-dev zip libssl-dev
+    #. wget https://www.python.org/ftp/python/3.6.10/Python-3.6.10.tgz
+    #. tar xf Python-3.6.10.tgz
+    #. cd Python-3.6.10
+    #. ./configure —prefix=/opt/python3.6.10
+    #. make
+    #. sudo make install
+    #. sudo ln -s /opt/python3.6.10/bin/python3.6 /usr/local/bin/python3.6
+    #. sudo ln -s /opt/python3.6.10/bin/pip3.6 /usr/local/bin/pip
+
+
+RDSの構築
+========
+:DB:    8.0.19
+
+RDSを作成しEC2インスタンスでの実行手順
+
+    * sudo apt -y install mysql-client default-libmysqlclient-dev
+
+データベースの接続確認
+
+    * mysql -h { database endpoint } -u rootname --password="password"
+
+データベーステーブルの作成
+
+    * sql> CREATE DATABASE startlens
+
+
+ソースコードのデプロイ
+===================
+EC2のvenv仮想環境で下記を実行
+
+    * pip install -r requirements/test.txt
+    * pip install wheel mysqlclient django-environ django-storages boto3
+    * mkdir /var/log/startlens_django
+    * BASE_DIRディレクトリに .envファイルを作成し config.setting.test で読み込む環境変数を定義する
+    * S3の作成とS3fullaccessユーザーをIAMで作成して .envにEC2からS3へアクセスするためのアクセスキーとシークレットキーを設定する
+
+
+Djangoの設定
+===========
+管理者アカウトを作成する
+
+    * python manage.py create superuser
+
+静的ファイルの配置
+
+    * sudo mkdir /var/www
+    * sudo mkdir /var/www/startlens_django
+    * sudo chown ubuntu:www-data /var/www/startlens_django
+    * python manage.py collectstatic --noinput
+
+
+アプリケーションサーバーとリバースプロキシの設定
+=========================================
+:nginx:    1.14.0
+:gunicorn:    20.0.4
+
+アプリケーションサーバーとしてgunicornをセットアップする
+
+    * pip install gunicorn
+    * gunicorn --daemon --bind=0.0.0.0:8000 config.wsgi
+
+デーモンで起動しているgunicornを停止する場合
+
+    * sudo lsof -i:8000
+    * sudo kill -9 PID
+
+リバースプロキシとしてnginxのインストールと設定
+
+    * sudo apt install -y nginx
+    * sudo systemctl enable nginx
+    * mv config/startlens /etc/nginx/site-available/startlens
+    * sudo ln -s /etc/nginx/sites-available/startlens /etc/nginx/sites-enabled/
+    * sudo unlink /etc/nginx/site-enabled/default
+    * sudo nginx -t
+    * sudo systemctl relaod nginx
+
---
+++ lambda_function.py
@@ -0,0 +1,144 @@
+import boto3
+import os
+import json
+import sys
+import uuid
+from urllib.parse import unquote_plus
+from traceback import print_exc
+from PIL import Image
+import PIL.Image
+from PIL.ExifTags import TAGS
+
+
+# s3クライアントの取得
+s3_client = boto3.client('s3')
+
+
+def resize_image(image):
+    """
+        Pillowによる画像のリサイズ処理
+    """
+    width, height = image.size
+    square_size = min(width, height)
+
+    if width > height:
+        # 横方向に切り取る
+        top = 0
+        bottom = square_size
+        left = (width - square_size) / 2
+        right = left + square_size
+        box = (left, top, right, bottom)
+    else:
+        # 縦方向に切り取る
+        left = 0
+        right = square_size
+        top = (height - square_size) / 2
+        bottom = top + square_size
+        box = (left, top, right, bottom)
+
+    image = image.crop(box)
+    # 600pxの正方形に調整
+    thumbnail_size = (600, 600)
+    image.thumbnail(thumbnail_size)
+    return image
+
+
+def _exif(file_path):
+    try:
+        # JSON生成
+        def _format_bytes(obj_):
+            res = {}
+            for key_, value_ in obj_.items():
+                if isinstance(value_, bytes):
+                    res[key_] = "{}".format(value_)
+                elif isinstance(value_, dict):
+                    res[key_] = _format_bytes(value_)
+                else:
+                    res[key_] = value_
+            return res
+
+        # Exifの読み取り
+        with Image.open(file_path) as f:
+            exif_ = f._getexif()
+        info_ = {}
+        for key_ in exif_.keys():
+            tag_ = TAGS.get(key_, key_)
+            if tag_ in ["MakerNote", "UserComment"]:
+                continue
+            info_[tag_] = exif_[key_]
+
+        return _format_bytes(info_)
+
+    except AttributeError:
+        # exif_が存在しない場合の例外処理
+        return {}
+    except BaseException:
+        raise
+
+
+def list_exif(file_path):
+    # jpeg以外のファイルの確認と処理
+    path_ = os.path.normpath(file_path)
+    buf_ = {"path": path_,
+            "file": os.path.basename(path_)}
+    # Exifデータの追加
+    buf_["exif"] = _exif(path_)
+    return buf_
+
+
+def remove_exif(file_path, resized_path):
+    """
+        PillowによるExif削除及び回転処理
+    """
+    # ファイルパス、ファイル名、Exif情報の取得
+    item = list_exif(file_path)
+    # Exif情報が存在するとき
+    if item["exif"] != {}:
+        orientation_ = item["exif"]["Orientation"]
+        # Exifにより画像が回転しているとき
+        if orientation_ > 1:
+            print("save file: {} (orientation:{})...".format(item["path"], orientation_))
+            trans_ = [0, 3, 1, 5, 4, 6, 2][orientation_ - 2]
+            with Image.open(item["path"]) as image_:
+                # 正しい方向へ画像を回転処理
+                image_ = image_.transpose(trans_)
+                # 600px - 600pxにリサイズ
+                image_ = resize_image(image_)
+                # アップロード用に一時保存
+                image_.save(resized_path)
+
+
+def lambda_handler(event, context):
+    """
+        Lambdaから最初に呼ばれるハンドラ関数
+    """
+    for record in event['Records']:
+        print("record: {}".format(record))
+        bucket = record['s3']['bucket']['name']
+        # 引数からS3のKey(フォルダ名/ファイル名)を抽出
+        key = unquote_plus(record['s3']['object']['key'])
+        splitkey = key.split('/')
+        # thumbnails/ファイル名のパスを作成
+        # newkey = key.replace(splitkey[0], 'thumbnails')
+        tmpkey = key.replace('/', '')
+        # S3からダウンロードしたファイルの保存先を設定
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
+        # 加工したファイルの一時保存先を設定
+        upload_path = '/tmp/resized-{}'.format(tmpkey)
+        # S3からファイルをダウンロード
+        s3_client.download_file(bucket, key, download_path)
+        # Exif削除と回転処理
+        remove_exif(download_path, upload_path)
+        # resize_image(download_path, upload_path)
+        # 処理後のファイルをS3にアップロード
+        s3_client.upload_file(upload_path, bucket, splitkey)
+
+
+if __name__ == "__main__":
+    # exif削除処理テスト
+    remove_exif('./image/d5f4e21c-a10c-4c0d-9f79_78QE6YA.jpg')
+
+    # 画像のリサイズテスト
+    with Image.open('./image/d5f4e21c-a10c-4c0d-9f79_78QE6YA.jpg') as image:
+        image = resize_image(image)
+        image.save('./image/d5f4e21c-a10c-4c0d-9f79_78QE6YA.jpg')
